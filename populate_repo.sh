e9c472326c41f8cba5cd9b16be1fc00c38238261
Kevin Thomas <kevin@mytechnotalent.com>
2022-10-15 18:44:55 -0400

    Updated populate_repo

--- populate_repo.sh
+++ populate_repo.sh
@@ -5,6 +5,8 @@ cd ..
 echo enter folder name:
 read folder_name
 mkdir $folder_name
+cp mp-template/.gitignore
+cp mp-template/LICENSE
 cp mp-template/.vimrc $folder_name
 cp mp-template/requirements.txt $folder_name
 cp mp-template/bare_flash.sh $folder_name
@@ -13,4 +15,6 @@ cp mp-template/copy_files_to_flash.sh $folder_name
 cp mp-template/init.sh $folder_name
 cd $folder_name
 git init .
+git config --global init.defaultBranch main
+git branch -m main
 ./init.sh
