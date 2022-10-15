7932debe541bbcaf4915318924d5263f9b35f221
Kevin Thomas <kevin@mytechnotalent.com>
2022-10-15 18:12:43 -0400

    Added populate_repo

---
+++ populate_repo.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+rm -rf mp-template
+cd ..
+echo enter folder name:
+read folder_name
+mkdir $folder_name
+cp mp-template/.vimrc $folder_name
+cp mp-template/requirements.txt $folder_name
+cp mp-template/bare_flash.sh $folder_name
+cp mp-template/build.sh $folder_name
+cp mp-template/copy_files_to_flash.sh $folder_name
+cp mp-template/init.sh $folder_name
+cd $folder_name
+git init .
+./init.sh
