9bd11ecdde3ea9e3adb074beb2dd91f2a49d6632
Kevin Thomas <kevin@mytechnotalent.com>
2022-10-16 19:39:49 -0400

    Changed paths to populate_repo.sh

--- populate_repo.sh
+++ populate_repo.sh
@@ -5,14 +5,14 @@ cd ..
 echo enter folder name:
 read folder_name
 mkdir $folder_name
-cp mp-template/.gitignore $folder_name
-cp mp-template/LICENSE $folder_name
-cp mp-template/.vimrc $folder_name
-cp mp-template/requirements.txt $folder_name
-cp mp-template/flash.sh $folder_name
-cp mp-template/build.sh $folder_name
-cp mp-template/copy_files_to_flash.sh $folder_name
-cp mp-template/init.sh $folder_name
+cp mp-pico-project-template/.gitignore $folder_name
+cp mp-pico-project-template/LICENSE $folder_name
+cp mp-pico-project-template/.vimrc $folder_name
+cp mp-pico-project-template/requirements.txt $folder_name
+cp mp-pico-project-template/flash.sh $folder_name
+cp mp-pico-project-template/build.sh $folder_name
+cp mp-pico-project-template/copy_files_to_flash.sh $folder_name
+cp mp-pico-project-template/init.sh $folder_name
 cd $folder_name
 git init .
 git config --global init.defaultBranch main
