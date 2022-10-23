c1600932c324bc034308aa8a60cb2c0f39e1b9af
Kevin Thomas <kevin@mytechnotalent.com>
2022-10-23 15:23:08 -0400

    Update populate_repo.sh

--- populate_repo.sh
+++ populate_repo.sh
@@ -11,6 +11,7 @@ cp mp-pico-project-template/requirements.txt $folder_name
 cp mp-pico-project-template/flash.sh $folder_name
 cp mp-pico-project-template/build.sh $folder_name
 cp mp-pico-project-template/copy_files_to_flash.sh $folder_name
+cp mp-pico-project-template/populate_submodules_on_clone.sh $folder_name
 cp mp-pico-project-template/init.sh $folder_name
 cd $folder_name
 git init .
