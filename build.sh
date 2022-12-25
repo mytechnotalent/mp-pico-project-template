918021b34a3796350e1901964d639c99dd8fac58
Kevin Thomas <kevin@mytechnotalent.com>
2022-12-25 13:33:33 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 . venv/bin/activate
+cp src/main.py micropython/ports/rp2/modules/
 cd micropython
 git checkout master
 git pull origin master
@@ -10,6 +11,7 @@ make -C ports/rp2 submodules
 make clean -C mpy-cross
 make -C mpy-cross
 cd ports/rp2
+make clean
 make
 cp build-PICO/*.elf ../../../
 cp build-PICO/*.uf2 ../../../
